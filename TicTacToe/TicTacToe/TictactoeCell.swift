8153f480c831ed5d5d481db472183dc0fea93f31
MAC Home <machome@MACs-iMac.local>
2017-03-01 14:42:11 +0300

    Init commit

---
+++ TicTacToe/TicTacToe/TictactoeCell.swift
@@ -0,0 +1,19 @@
+//
+//  TictactoeCell.swift
+//  TicTacToe
+//
+//  Created by MAC Home on 01.03.17.
+//  Copyright © 2017 HSE. All rights reserved.
+//
+
+import UIKit
+
+class TictactoeCell: UICollectionViewCell {
+
+    
+    
+    
+    
+    
+}
+
---
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by MAC Home on 01.03.17.
+//  Copyright © 2017 HSE. All rights reserved.
+//
+
+import UIKit
+
+fileprivate let reuseIdentifier = "tictactoeitem"
+fileprivate let screenWidth = UIScreen.main.bounds.width
+
+
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupCollectionViewCells()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Setup
+    
+    func setupCollectionViewCells() {
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 20, right: 0)
+        
+        let padding: CGFloat = 10
+        let itemWidth = screenWidth/3 - padding
+        let itemHeight = screenWidth/3 - padding
+        
+        layout.itemSize = CGSize(width: itemWidth, height: itemHeight)
+        layout.minimumLineSpacing = 10
+        layout.minimumInteritemSpacing = 10
+        
+        collectionView.collectionViewLayout = layout
+    }
+    
+    // MARK: UICollectionViewDataSource
+    
+    // Overall number of items in data list
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 9
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as? TictactoeCell else { fatalError()}
+
+    // MARK: - UICollectionViewDelegate
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("touched")
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        updateCell(having: indexPath, selected: false, outOfStock: false)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didHighlightItemAt indexPath: IndexPath) {
+        updateCell(having: indexPath, selected: true, outOfStock: false)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didUnhighlightItemAt indexPath: IndexPath) {
+        updateCell(having: indexPath, selected: false, outOfStock: false)
+    }
+    
+    func updateCell(having indexPath: IndexPath, selected: Bool, outOfStock: Bool) {
+        
+        let selectedBackgroundColor = UIColor(red: 41/255.0, green: 211/255.0, blue: 241/255.0, alpha: 1.0)
+        let defaultBackgroundColor = UIColor(red: 27/255.0, green: 32/255.0, blue: 36/255.0, alpha: 1.0)
+    }
+
+    
+
+
+
+}
+
