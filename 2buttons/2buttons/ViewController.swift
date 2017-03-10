55e8efe8f367845ffd081caefe3ca1ee9caf2f28
Alex L <lebedev.alex69@gmail.com>
2017-03-10 17:09:00 +0300

    fixing layout

--- 2buttons/2buttons/ViewController.swift
+++ 2buttons/2buttons/ViewController.swift
@@ -12,8 +12,8 @@ class ViewController: UIViewController {
     let greenColor = UIColor(red: 85/255.0, green: 176/255.0, blue: 112/255.0, alpha: 1.0)
     let redColor =  UIColor(red: 223/255.0, green: 86/255.0, blue: 94/255.0, alpha: 1.0)
     let grayColor = UIColor(red: 204/255.0, green: 204/255.0, blue: 204/255.0, alpha: 1.0)
-    let font = UIFont(name: "HelveticaNeue-Bold", size: 45)
-    let basicFont = UIFont(name: "System-Regular", size: 25)
+    let font = UIFont(name: "HelveticaNeue-Bold", size: 75)
+    let basicFont = UIFont(name: "System-Regular", size: 40)
     var able = true
     
     @IBOutlet weak var button1: UIButton!
@@ -21,29 +21,32 @@ class ViewController: UIViewController {
     @IBOutlet weak var border: UIImageView!
     
     
+    func changeColor(button1: UIButton, button2: UIButton, color: UIColor) {
+        button1.setTitleColor(UIColor.white, for: .normal)
+        button1.titleLabel!.font = font
+        button1.backgroundColor = color
+        button2.backgroundColor = color
+        border.backgroundColor = color
+        button2.setTitleColor(color, for: .normal)
+        able = false
+    }
+    
+    
     @IBAction func Button1(_ sender: UIButton) {
         if able {
-        
-            button1.setTitleColor(UIColor.white, for: .normal)
-            button1.titleLabel!.font = font
-            button1.backgroundColor = greenColor
-            button2.backgroundColor = greenColor
-            border.backgroundColor = greenColor
-            button2.setTitleColor(greenColor, for: .normal)
-            able = false
+            changeColor(button1: button1, button2: button2, color: greenColor)
+            
+        } else {
+            newGame()
         }
     }
     
     @IBAction func Button2(_ sender: UIButton) {
         if able{
+            changeColor(button1: button2, button2: button1, color: redColor)
             
-            button2.setTitleColor(UIColor.white, for: .normal)
-            button2.titleLabel!.font = font
-            button2.backgroundColor = redColor
-            button1.backgroundColor = redColor
-            border.backgroundColor = redColor
-            button1.setTitleColor(redColor, for: .normal)
-            able = false
+        } else {
+            newGame()
         }
     }
     
